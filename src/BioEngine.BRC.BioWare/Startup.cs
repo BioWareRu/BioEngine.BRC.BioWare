299fe5dbf40a8369446284c11233f88f243d7dd7
George <sonicgd@gmail.com>
2019-05-03 19:20:12 +0500

    Add patreon widget

--- src/BioEngine.BRC.BioWare/Startup.cs
+++ src/BioEngine.BRC.BioWare/Startup.cs
@@ -1,17 +1,32 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
+using BioEngine.BRC.BioWare.Patreon;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BioEngine.BRC.BioWare
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+            services.AddSingleton<PatreonApiHelper>();
+            services.Configure<PatreonConfig>(o =>
+            {
+                o.ServiceUrl = new Uri(_configuration["BE_PATREON_SERVICE_URL"]);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -21,11 +36,7 @@ namespace BioEngine.BRC.BioWare
                 app.UseDeveloperExceptionPage();
             }
 
-            var supportedCultures = new[]
-            {
-                new CultureInfo("ru-RU"),
-                new CultureInfo("ru")
-            };
+            var supportedCultures = new[] {new CultureInfo("ru-RU"), new CultureInfo("ru")};
 
             app.UseRequestLocalization(new RequestLocalizationOptions
             {
@@ -35,7 +46,7 @@ namespace BioEngine.BRC.BioWare
             });
 
             app.UseStaticFiles();
-            
+
             app.UseMvc();
         }
     }
