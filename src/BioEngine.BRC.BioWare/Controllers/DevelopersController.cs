cb107e3fbaa85f63704597b6556b7cf14cd30391
George <sonicgd@gmail.com>
2019-05-06 11:37:07 +0500

    fix BioWareRu/BioEngine.BRC.BioWare#18

--- src/BioEngine.BRC.BioWare/Controllers/DevelopersController.cs
+++ src/BioEngine.BRC.BioWare/Controllers/DevelopersController.cs
@@ -1,5 +1,6 @@
 using BioEngine.BRC.Domain.Entities;
 using BioEngine.Core.Site;
+using BioEngine.Core.Web;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BioEngine.BRC.BioWare.Controllers
@@ -7,7 +8,7 @@ namespace BioEngine.BRC.BioWare.Controllers
     [Route("/developers")]
     public class DevelopersController : SiteController<Developer>
     {
-        public DevelopersController(SiteControllerContext<Developer> context) : base(context)
+        public DevelopersController(BaseControllerContext<Developer> context) : base(context)
         {
         }
     }
--- src/BioEngine.BRC.BioWare/Startup.cs
+++ src/BioEngine.BRC.BioWare/Startup.cs
@@ -1,53 +1,25 @@
 ﻿using System;
-using System.Globalization;
 using BioEngine.BRC.BioWare.Patreon;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc;
+using BioEngine.Core.Site;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BioEngine.BRC.BioWare
 {
-    public class Startup
+    public class Startup : BioEngineStartup
     {
-        private readonly IConfiguration _configuration;
-
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration) : base(configuration)
         {
-            _configuration = configuration;
         }
 
-        public void ConfigureServices(IServiceCollection services)
+        public override void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+            base.ConfigureServices(services);
             services.AddSingleton<PatreonApiHelper>();
             services.Configure<PatreonConfig>(o =>
             {
-                o.ServiceUrl = new Uri(_configuration["BE_PATREON_SERVICE_URL"]);
+                o.ServiceUrl = new Uri(Configuration["BE_PATREON_SERVICE_URL"]);
             });
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            var supportedCultures = new[] {new CultureInfo("ru-RU"), new CultureInfo("ru")};
-
-            app.UseRequestLocalization(new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture("ru-RU"),
-                SupportedCultures = supportedCultures,
-                SupportedUICultures = supportedCultures
-            });
-
-            app.UseStaticFiles();
-
-            app.UseMvc();
         }
     }
 }
